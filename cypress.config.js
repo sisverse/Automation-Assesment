715f6230510e0aecaac9f79afb4ec7feaff3b12a
Sis Maharjan <sismaharjan@gmail.com>
2023-12-07 14:07:24 +0545

    removed configurations that is not required to run the project

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://practicetestautomation.com/",
     chromeWebSecurity: false,
-    experimentalModifyObstructiveThirdPartyCode: true, // this configuration prevents authentication workflow from entering an infinite redirect loop.
-    experimentalOriginDependencies: true,
     pageLoadTimeout: 100000,
     defaultCommandTimeout: 25000,
     viewportWidth: 1366,
